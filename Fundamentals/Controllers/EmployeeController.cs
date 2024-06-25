7baf15ca04ce8abab3177e65d4790be6465f335c
Bunyamin Yavuz <1b.yavuz07@gmail.com>
2024-06-25 14:23:00 +0300

    Add initial controller actions for returning text messages and views.

--- Fundamentals/Controllers/EmployeeController.cs
+++ Fundamentals/Controllers/EmployeeController.cs
@@ -4,8 +4,18 @@ namespace Fundamentals.Controllers
 {
     public class EmployeeController : Controller
     {
-        public String Index() {
+        public String Index()
+        {
             return "Ben is coming!";
+        }
+
+        public ViewResult Index2()
+        {
+            return View("Index");
+        }
+
+        public IActionResult Index3() {
+            return Content("Employee");
         }
     }
 }
